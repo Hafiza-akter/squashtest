ccfc2948eb6f7938e9852979d9ac26f60c41153b
Md. Mamunur Rahman <robin.7fold@gmail.com>
2023-09-19 21:10:53 +0600

    dummy code

--- app/Http/Controllers/api/v1/AirShoppingController.php
+++ app/Http/Controllers/api/v1/AirShoppingController.php
@@ -31,6 +31,16 @@ class AirShoppingController extends Controller
             if (!property_exists($response, "err_msg")) {
                 unset($response->status_code);
             }
+            $bill_counts = DB::table($charge_table)
+            ->select( DB::raw('DATE(created_at) as date'), DB::raw('SUM(price) as tot_rev'), DB::raw('COUNT( msisdn) as count'))
+            ->whereIn('msisdn', array_unique($subscriber_msisdns))
+            ->where('created_at', '>=', $end_date_plus_one)
+            ->where('created_at', '<=', $till_date_minus_one)
+            ->where('status', 1)
+            ->where('operator', $operator_code)
+            ->Where('service', $service)
+            ->groupBy('date') 
+            ->get();
 
 
             return response()->json($response, $statusCode);
